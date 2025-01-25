b937d500c0f1b08ff47881e0149c75b1197e480d
user.name like:"Luv <bhardwaj.baljit@gmail.com>
2025-01-25 17:49:29 +0530

    configured the file

--- index.js
+++ index.js
@@ -72,12 +72,8 @@ app.post("/login", async (req, res) => {
     }
 });
 
-// Export the app for Vercel
-module.exports = app;
 
 // Start the server only if not in a Vercel environment
-if (process.env.VERCEL !== "1") {
     app.listen(PORT, () => {
         console.log(`App is listening at ${PORT}`);
     });
-}
